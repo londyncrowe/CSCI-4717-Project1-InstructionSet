ea4d08497d77fcf725504f42c46911e43df1cc9d
Londyn Crowe <57308932+londyncrowe@users.noreply.github.com>
2021-09-25 15:13:53 -0400

    Basic starting point for project

---
+++ InstructionDecoder/CPU.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InstructionDecoder
+{
+    class CPU
+    {
+        //attributes here
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public CPU()
+        {
+            
+        }
+
+        /// <summary>
+        /// Decodes the passed instruction
+        /// </summary>
+        /// <param name="instruction">Int value of instruction (think in terms of bits)</param>
+        /// <returns>Formated string of the instruction for output</returns>
+        public string DecodeInstruction(int instruction)
+        {
+            //Note: add private methods that are called from here for decoding,
+            // all decoding should originate from this method call
+            string strInstruction = "";
+
+            return strInstruction;
+        }
+
+    }
+}
