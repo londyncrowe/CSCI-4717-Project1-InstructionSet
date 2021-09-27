f6fd9b4f85bf8924a1d4b6ed853fc5b44b09b9ea
adhubbard <70924233+adhubbard@users.noreply.github.com>
2021-09-27 19:37:06 -0400

    GUI initial

--- InstructionDecoder/Form1.cs
+++ InstructionDecoder/Form1.cs
@@ -17,5 +17,14 @@ namespace InstructionDecoder
             InitializeComponent();
         }
 
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
